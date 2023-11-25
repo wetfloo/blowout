f6dcd1a4bee5e842fdf71f3b6361bf8525abe5af
wetfloo <yahahaha.1234567890@gmail.com>
2023-11-25 08:41:31 +0700

    filter empty lines, return Err when there are no valid values

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,10 @@ mod regex;
 mod speed;
 mod unit;
 
+#[derive(Debug, thiserror::Error)]
+#[error("no valid values found in the file")]
+struct NoValues;
+
 fn main() -> anyhow::Result<()> {
     let args = Args::parse();
 
@@ -18,11 +22,18 @@ fn main() -> anyhow::Result<()> {
     let reader = BufReader::new(file);
 
     let unit = args.unit.try_into()?;
-    let _speeds: Vec<_> = reader
+    let speeds: Vec<_> = reader
         .lines()
+        .filter(|line_res| match line_res {
+            Ok(line_res) => !line_res.trim().is_empty(),
+            Err(_) => true,
+        })
         .map(|line_res| speed::get_speed(&unit, &line_res?))
         .filter_map(|speed_res| speed_res.ok())
         .collect();
+    if speeds.is_empty() {
+        return Err(NoValues.into());
+    }
 
     Ok(())
 }

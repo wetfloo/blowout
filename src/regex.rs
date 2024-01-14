72080cf716f5b56ff795b2a969115fe39bb46b21
wetfloo <yahahaha.1234567890@gmail.com>
2024-01-14 23:11:31 +0700

    remove regex parser

--- src/regex.rs
+++ src/regex.rs
@@ -1,21 +1,7 @@
-use once_cell::sync::{Lazy, OnceCell as SyncOnceCell};
+use once_cell::sync::Lazy;
 use regex::Regex;
-
-use crate::unit::MeasurementUnit;
-
-const REGEX_STR: &str = r"(\d+(?:,\d+)?)\s*";
-/// This is used for capturing actual input values.
-static REGEX: SyncOnceCell<Regex> = SyncOnceCell::new();
 
 const UNITS_REGEX_STR: &str = r"\w+/\w+";
 /// This is used to validate if the units
 /// are entered in the correct format or not.
 pub static UNITS_REGEX: Lazy<Regex> = Lazy::new(|| Regex::new(UNITS_REGEX_STR).unwrap());
-
-pub fn get_regex(unit: &MeasurementUnit) -> anyhow::Result<&'static Regex> {
-    let mut buf = REGEX_STR.to_string();
-    buf.push_str(&unit.0.clone());
-    let regex: Regex = buf.try_into()?;
-
-    Ok(REGEX.get_or_init(|| regex))
-}
--- src/unit.rs
+++ src/unit.rs
@@ -24,45 +24,7 @@ impl TryFrom<String> for MeasurementUnit {
 
 #[cfg(test)]
 mod tests {
-    use crate::{regex::get_regex, unit::MeasurementUnit};
-
-    #[test]
-    fn test_regex() {
-        let input_speed = "6900";
-        let input_unit = "km/h";
-        let input = format!(r"1 18,2°C 40,7% {input_speed} {input_unit} 15.06.2023\11:06:11 ");
-
-        let unit = MeasurementUnit(input_unit.to_string());
-        let regex = get_regex(&unit);
-        let captures = regex.ok().and_then(|regex| regex.captures(&input)).unwrap();
-        assert_eq!(1, captures.iter().skip(1).len());
-        assert_eq!(
-            Some(input_speed),
-            captures
-                .iter()
-                .skip(1)
-                .find_map(|capture| capture.map(|c| c.as_str()))
-        );
-    }
-
-    #[test]
-    fn test_regex_fraction() {
-        let input_speed = "0,420";
-        let input_unit = "km/h";
-        let input = format!(r"1 18,2°C 40,7% {input_speed} {input_unit} 15.06.2023\11:06:11 ");
-
-        let unit = MeasurementUnit(input_unit.to_string());
-        let regex = get_regex(&unit);
-        let captures = regex.ok().and_then(|regex| regex.captures(&input)).unwrap();
-        assert_eq!(1, captures.iter().skip(1).len());
-        assert_eq!(
-            Some(input_speed),
-            captures
-                .iter()
-                .skip(1)
-                .find_map(|capture| capture.map(|c| c.as_str()))
-        );
-    }
+    use crate::unit::MeasurementUnit;
 
     #[test]
     fn test_units_valid() {
